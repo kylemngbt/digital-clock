5713f182a19919f7d66cad3830b0823fded93b7e
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-27 15:00:58 +0800

    feat: create the initial update time func

--- javascript/script.js
+++ javascript/script.js
@@ -2,5 +2,10 @@ const hrs = document.querySelector("#hrs");
 const min = document.querySelector("#min");
 const sec = document.querySelector("#sec");
 
-let currentTime = new Date();
+setInterval(() => {
+  let currentTime = new Date();
 
+  hrs.textContent = currentTime.getHours();
+  min.textContent = currentTime.getMinutes();
+  sec.textContent = currentTime.getSeconds();
+},1000)
