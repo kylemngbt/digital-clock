c50af9ab349b9a7fb8bf8fb59053f80b1ae2b6ec
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-27 15:02:39 +0800

    feat: display the single digits with additional 0

--- javascript/script.js
+++ javascript/script.js
@@ -5,7 +5,7 @@ const sec = document.querySelector("#sec");
 setInterval(() => {
   let currentTime = new Date();
 
-  hrs.textContent = currentTime.getHours();
-  min.textContent = currentTime.getMinutes();
-  sec.textContent = currentTime.getSeconds();
+  hrs.textContent = (currentTime.getHours() < 10 ? "0" : "") + currentTime.getHours();
+  min.textContent = (currentTime.getMinutes() < 10 ? "0" : "") + currentTime.getMinutes();
+  sec.textContent = (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 },1000)
